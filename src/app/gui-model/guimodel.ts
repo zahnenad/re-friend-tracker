4e0f05cb5769f8097834ea1e1110b47c8530867a
zahnenad <45898614+zahnenad@users.noreply.github.com>
2018-12-15 16:17:04 +0100

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Nadine Friend Tracker",
+            "title": "Nadines Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
